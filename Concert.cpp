08b2401506798d54a66964a38fe38f49c48684b6
Abdullah-crns <u2023037@giki.edu.pk>
2026-01-02 12:41:31 +0500

    Updated

--- Concert.cpp
+++ Concert.cpp
@@ -37,7 +37,7 @@ struct Concert_Reservation
     int age; // Added age field
     string ticketID;
     string time;
-    string singer_name;
+    string singer_nam;
     string type;
     string date;
     double price;
